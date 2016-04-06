2964725014f55d3f8a5c11619c690b1b9639a576
Dave Paola <dpaola2@gmail.com>
2016-04-06 16:26:03 -0700

    ability to delete db connections, closes #14

--- app/controllers/database_connection_controller.rb
+++ app/controllers/database_connection_controller.rb
@@ -4,7 +4,7 @@ class DatabaseConnectionController < ApplicationController
   end
 
   def show
-    @connection = DatabaseConnection.find(params[:id])
+    find_database_connection
   end
 
   def create
@@ -29,8 +29,27 @@ class DatabaseConnectionController < ApplicationController
   end
 
   def destroy
+    find_database_connection
+    if @connection.saved_queries.count > 0
+      flash[:error] = "Please remove all saved queries associated with this connection first."
+      redirect_to database_connection_path(@connection)
+    else
+      if @connection.destroy
+        flash[:notice] = "Database connection deleted."
+        redirect_to database_connection_index_path
+      else
+        flash[:error] = "Unable to delete database connection."
+        redirect_to database_connection_path(@connection)
+      end
+    end
   end
 
   def new
+  end
+
+  private
+
+  def find_database_connection
+    @connection = DatabaseConnection.find(params[:id])
   end
 end
