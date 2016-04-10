4b80d2ff08bd3a7aadf1b487041c73c3a269cfc9
Dave Paola <dpaola2@gmail.com>
2016-04-10 12:19:48 -0700

    renamed some things to queryclips in the rails app, refs #39

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'csv'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Duplo
+module QueryClips
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
