d933fa6fbf4887b1f144ad071d3f2dbefc68d82f
Dave Paola <dpaola2@gmail.com>
2016-07-21 16:46:12 -0700

    schema explorer for postgres

--- app/models/database_connection.rb
+++ app/models/database_connection.rb
@@ -1,3 +1,11 @@
 class DatabaseConnection < ActiveRecord::Base
   has_many :saved_queries
+
+  def tables
+    SchemaExplorer.new(database_connection: self).tables
+  end
+
+  def table_schema(table)
+    SchemaExplorer.new(database_connection: self).table_schema(table)
+  end
 end
--- lib/schema_explorer.rb
+++ lib/schema_explorer.rb
@@ -1,9 +1,28 @@
 class SchemaExplorer
-  def initialize(database_connection)
-    @database_connection = database_connection
+  def initialize(options = {})
+    @database_connection = options[:database_connection]
+    raise "No database connection specified." if @database_connection.nil?
+
+    case @database_connection.dialect
+    when 'PostgreSQL'
+      dialect = PostgresqlDialect
+    when 'MySQL'
+      dialect = MysqlDialect
+    else
+      raise "Invalid dialect: #{@database_connection.dialect}"
+    end
+
+    @dialect = dialect.new(
+      database_connection: @database_connection
+    )
+
   end
 
   def tables
-    
+    @dialect.tables[:rows].collect {|r| r.first }
+  end
+
+  def table_schema(table)
+    @dialect.table_schema(table)
   end
 end
