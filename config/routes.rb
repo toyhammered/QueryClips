c1db86b84b9f3f62be82b12a26073479d2a57f29
Dave Paola <dpaola2@gmail.com>
2016-07-22 17:47:24 -0700

    login, logout, signup works, refs #13

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get '/signup',     to: 'users#new'
+  get '/login',      to: 'sessions#new'
+  post '/login',     to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
   resources :database_connection
   resources :queries do
     get :run, on: :collection
   end
-
-  get '/signup', to: 'users#new'
   resources :users, only: [:new, :create]
 
   root 'queries#index'
