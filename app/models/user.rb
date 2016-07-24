a934eae47c312874c03c7cdf43e826515edce1f8
Dave Paola <dpaola2@gmail.com>
2016-07-24 10:52:45 -0700

    users & database_connection relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   validates :password, length: { minimum: 6 }
   has_secure_password
+  has_many :database_connections
 end
