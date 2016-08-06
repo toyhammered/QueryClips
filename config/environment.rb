4e0bcec928fd4fa3f2b614a716004a77cd815aff
Dave Paola <dpaola2@gmail.com>
2016-08-06 11:18:14 -0700

    fixes bad host name, refs #111

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  address: ENV['SMTP_ADDRESS'],
+  address: ENV['SMTP_HOST'],
   port:    ENV['SMTP_PORT'],
   authentication: :plain,
   user_name: ENV['SENDGRID_USERNAME'],
