7538eede533ca4cba070e421187d2f194d00c77e
toyhammered <daniel@rassiner.com>
2017-03-16 14:12:23 -0700

    add basic logic behind remember me. WIP: implement the button on forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
 
   validates :password, length: { minimum: 6 }, on: :create
 
   has_secure_password
-  
+
   has_many :database_connections
   has_many :saved_queries
 
@@ -14,5 +16,33 @@ class User < ActiveRecord::Base
 
   def query_count
     saved_queries.count
+  end
+
+
+  # Returns the hash digest of the given string.
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token.
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160820191859) do
+ActiveRecord::Schema.define(version: 20170316191809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20160820191859) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",           default: false, null: false
+    t.string   "remember_digest"
   end
 
   create_table "visualizations", force: true do |t|
